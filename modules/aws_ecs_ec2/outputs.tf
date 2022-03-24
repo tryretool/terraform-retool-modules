a2a6f581dc671f4b42ce8ee6c9f7407b7fcd7af3
Evan O'Brien <evanbobrien@gmail.com>
2022-03-24 20:04:18 +0000

    Add ALB zone ID output

--- modules/aws_ecs_ec2/outputs.tf
+++ modules/aws_ecs_ec2/outputs.tf
@@ -8,6 +8,14 @@ output "ecs_alb_arn" {
   description = "Retool ALB arn"
 }
 
+output "ecs_alb_target_group_arn" {
+  value = aws_lb_target_group.this.arn
+}
+
+output "ecs_alb_zone_id" {
+  value = aws_lb.this.zone_id
+}
+
 output "ecs_cluster_name" {
   value       = aws_ecs_cluster.this.name
   description = "Name of AWS ECS Cluster"
