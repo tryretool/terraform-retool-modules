91dcc45e678f444ca38504fdd3abe3acb522b971
Ernesto Sanchez <122387027+esanchezxev@users.noreply.github.com>
2025-03-24 19:44:20 -0700

    added tags variable to tag all taggable resources

--- modules/aws_ecs/temporal/secrets.tf
+++ modules/aws_ecs/temporal/secrets.tf
@@ -1,4 +1,3 @@
-
 resource "random_string" "temporal_aurora_password" {
   length  = var.secret_length
   special = false
@@ -8,6 +7,7 @@ resource "aws_secretsmanager_secret" "temporal_aurora_password" {
   name                    = "${var.deployment_name}-temporal-rds-password"
   description             = "This is the password for the Retool Temporal RDS instance"
   recovery_window_in_days = 0
+  tags                    = var.tags
 }
 
 resource "aws_secretsmanager_secret_version" "temporal_aurora_password" {
@@ -19,6 +19,7 @@ resource "aws_secretsmanager_secret" "temporal_aurora_username" {
   name                    = "${var.deployment_name}-temporal-rds-username"
   description             = "This is the username for the Retool Temporal RDS instance"
   recovery_window_in_days = 0
+  tags                    = var.tags
 }
 
 resource "aws_secretsmanager_secret_version" "temporal_aurora_username" {
